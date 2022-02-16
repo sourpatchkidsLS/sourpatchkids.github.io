fad4e6daf5a5ce29f2779e8717fd94d36151678a
sourpatchkidsLS <91155335+sourpatchkidsLS@users.noreply.github.com>
2022-02-15 21:28:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function changecolor(){
-    document.getElementById("NameHeading").style.color="purple";
+function turnpurple(){
+    document.getElementById("specs").style.color="#846DCF";
 }
+function turnpurple1(){
+    document.getElementById("specs1").style.color="#846DCF";
+}
+function turnpurple2(){
+    document.getElementById("specs2").style.color="#846DCF";
+}
+
+console.log("why u looking at console")
+alert(">welcome to my website<")
